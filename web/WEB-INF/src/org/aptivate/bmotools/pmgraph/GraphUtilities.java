46c38be1bc5c4209cb708a0dbca52c5e2d713346
Aptivate <info+pmgraph@aptivate.org>
2009-01-30 10:48:17 +0000

    git-svn-id: https://svn.code.sf.net/p/pmgraph/code/pmgraph@34 903098bd-d3be-4b5e-9df1-ec1ba492e1ff

--- web/WEB-INF/src/org/aptivate/bmotools/pmgraph/GraphUtilities.java
+++ web/WEB-INF/src/org/aptivate/bmotools/pmgraph/GraphUtilities.java
@@ -15,23 +15,24 @@ public abstract class GraphUtilities {
 	    "SELECT stamp_inserted, " +
 	    	   "SUM(IF(ip_dst LIKE ?, bytes, 0)) AS downloaded, " +
 	    	   "SUM(IF(ip_src LIKE ?, bytes, 0)) AS uploaded " +
-	    "FROM acct_v6 " +
+	     "FROM acct_v6 " + 
 	    "WHERE (ip_src LIKE ? XOR ip_dst LIKE ?) AND " +
-	          "stamp_inserted > ? AND " +
-	          "stamp_inserted < ? " +
+	          "stamp_inserted >= ? AND " +
+	          "stamp_inserted <= ? " +
 	    "GROUP BY stamp_inserted;";
 	
 	// Lists throughput per IP
 	public static final String THROUGHPUT_PER_IP =
 		"SELECT IF(ip_src LIKE ?, ip_src, ip_dst) AS local_ip, " +
 		       "SUM(IF(ip_dst LIKE ?, bytes, 0)) AS downloaded, " +
-		       "SUM(if(ip_src LIKE ?, bytes, 0)) AS uploaded " +
-		"FROM acct_v6 " +
+		       "SUM(if(ip_src LIKE ?, bytes, 0)) AS uploaded, " +
+		       "SUM(bytes) AS bytes_total " +
+		       "FROM acct_v6 " + 
 		"WHERE (ip_src LIKE ? XOR ip_dst LIKE ?) AND " +
-	          "stamp_inserted > ? AND " + 
-	          "stamp_inserted < ? " +
+	          "stamp_inserted >= ? AND " + 
+	          "stamp_inserted <= ? " +
 		"GROUP BY local_ip " +
-		"ORDER BY downloaded DESC;";
+		"ORDER BY bytes_total DESC;";
 	
 	// Lists throughput per IP per minute
 	public static final String THROUGHPUT_PER_IP_PER_MINUTE =
@@ -39,15 +40,16 @@ public abstract class GraphUtilities {
 		       "IF(ip_src LIKE ?, ip_src, ip_dst) AS local_ip, " +
 		       "SUM(IF(ip_dst LIKE ?, bytes, 0)) as downloaded, " +
 		       "SUM(IF(ip_src LIKE ?, bytes, 0)) as uploaded " +
-		"FROM acct_v6 " +
-		"WHERE (ip_src LIKE ? XOR ip_dst LIKE ?) AND " +
-		      "stamp_inserted > ? AND " + 
-		      "stamp_inserted < ? " +
+		       "FROM acct_v6 " + 
+		       "WHERE (ip_src LIKE ? XOR ip_dst LIKE ?) AND " +
+		      "stamp_inserted >= ? AND " + 
+		      "stamp_inserted <= ? " +
 		"GROUP BY stamp_inserted, local_ip;";
 	
 	public static Connection getConnection()
-		throws ClassNotFoundException, IllegalAccessException, 
-				InstantiationException, IOException, SQLException {
+	throws ClassNotFoundException, IllegalAccessException, InstantiationException, 
+		   IOException, SQLException 
+	{
 		Properties properties = new Properties();
 		InputStream stream = GraphFactory.class.
 		getResourceAsStream("/database.properties");
@@ -56,13 +58,14 @@ public abstract class GraphUtilities {
 
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		return DriverManager.getConnection(properties.getProperty("DatabaseURL"), 
-				properties.getProperty("DatabaseUser"),
-				properties.getProperty("DatabasePass"));
+		 	                               properties.getProperty("DatabaseUser"), 
+		 	                               properties.getProperty("DatabasePass"));
 	}
 
 	public static String getProperties()
-		throws ClassNotFoundException, IllegalAccessException, 
-				InstantiationException, IOException, SQLException {
+	throws ClassNotFoundException, IllegalAccessException, InstantiationException, 
+		   IOException, SQLException
+	{
 		Properties properties = new Properties();
 		InputStream stream = GraphFactory.class.
 		getResourceAsStream("/database.properties");
