bec08582c151d5a3a4471aff9617c84db0fba8ba
Aptivate <pmgraph-team@aptivate.org>
2009-04-29 15:39:40 +0000

    Rename TimeFormTest to TimeFormEntryTest (anne)

--- tests/org/aptivate/bmotools/pmgraph/AllTests.java
+++ tests/org/aptivate/bmotools/pmgraph/AllTests.java
@@ -20,7 +20,7 @@ public class AllTests extends TestCase
 		suite.addTest(GraphsTest.suite());
 		suite.addTest(LegendTest.suite());
 		suite.addTest(StartAndEndEntryTest.suite());
-		suite.addTest(TimeFormTest.suite());
+		suite.addTest(TimeFormEntryTest.suite());
 		suite.addTest(ResultEntryTest.suite());
 		suite.addTest(IPandPortEntryTest.suite());
 		suite.addTest(LegendTestPortView.suite());
